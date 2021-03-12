447f922ca7d1ab19ffc287adbe4ea5aa73e9703a
nerdvibe <6796782+nerdvibe@users.noreply.github.com>
2021-03-12 05:31:23 +0100

    created mina-ledger-js

---
+++ src/lib.ts
@@ -0,0 +1,327 @@
+import {
+  SignTransactionArgs,
+  TxType,
+  Transport,
+  SignTransactionResponse,
+  GetAddressResponse,
+  GetAppVersionesponse,
+} from "./types";
+import { Buffer } from "buffer/";
+const CLA = 0xe0;
+const INS = {
+  GET_VERSION: 0x01,
+  GET_ADDR: 0x02,
+  SIGN: 0x03,
+};
+const SW_OK = 0x9000;
+const SW_CANCEL = 0x6986;
+
+/**
+ * Mina App API
+ */
+
+export class MinaLedgerJS {
+  transport: Transport;
+
+  constructor(transport: Transport, scrambleKey: string = "carbonara") {
+    if (transport === null || typeof transport === "undefined") {
+      throw new Error("Transport cannot be empty");
+    }
+    transport.setScrambleKey(scrambleKey);
+    this.transport = transport;
+  }
+
+  protected pad(
+    n: number | string,
+    width: number = 3,
+    paddingValue: number | string = 0
+  ) {
+    return (String(paddingValue).repeat(width) + String(n)).slice(
+      String(n).length
+    );
+  }
+
+  protected asciiToHex(str: string) {
+    return Buffer.from(str, "ascii").toString("hex");
+  }
+
+  protected convertMemo(memo: string) {
+    const length = 32;
+    let charToAdd = length - memo.length;
+    let stringToReturn = memo;
+    while (charToAdd > 0) {
+      stringToReturn += "\x00";
+      charToAdd--;
+    }
+    return Buffer.from(stringToReturn, "utf8").toString("hex");
+  }
+
+  protected createTXApdu({
+    txType,
+    senderAccount,
+    senderAddress,
+    receiverAddress,
+    amount,
+    fee,
+    nonce,
+    validUntil = 4294967295,
+    memo = "",
+    networkId,
+  }: SignTransactionArgs) {
+    const senderBip44AccountHex = this.pad(senderAccount, 8);
+    const senderAddressHex = this.asciiToHex(senderAddress);
+    const receiverHex = this.asciiToHex(receiverAddress);
+    const amountHex = this.pad(amount.toString(16), 16);
+    const feeHex = this.pad(fee.toString(16), 16);
+    const nonceHex = this.pad(nonce, 8);
+    const validUntilHex = this.pad(validUntil.toString(16), 8);
+    const memoHex = this.convertMemo(memo);
+    const tagHex = this.pad(txType.toString(16), 2);
+    const networkIdHex = this.pad(networkId, 2);
+
+    // console.log("senderBip44AccountHex", senderBip44AccountHex);
+    // console.log("senderAddressHex", senderAddressHex);
+    // console.log("receiverHex", receiverHex);
+    // console.log("amountHex", amountHex);
+    // console.log("feeHex", feeHex);
+    // console.log("nonceHex", nonceHex);
+    // console.log("validUntilHex", validUntilHex);
+    // console.log("memoHex", memoHex);
+    // console.log("tagHex", tagHex);
+    // console.log("networkIdHex", networkIdHex);
+
+    const apduMessage =
+      senderBip44AccountHex +
+      senderAddressHex +
+      receiverHex +
+      amountHex +
+      feeHex +
+      nonceHex +
+      validUntilHex +
+      memoHex +
+      tagHex +
+      networkIdHex;
+
+    // console.log(apduMessage);
+
+    return apduMessage;
+  }
+
+  /**
+   * Get Mina address for a given account number.
+   *
+   * @param account int of the account number
+   * @param display optionally enable or not the display
+   * @return an object with a publicKey
+   * @example
+   * const result = await Mina.getAddress(1);
+   * const { publicKey, address, returnCode } = result;
+   */
+  async getAddress(account: number =0): Promise<GetAddressResponse> {
+    if (!Number.isInteger(account)) {
+      return {
+        publicKey: null,
+        returnCode: "-5",
+        message: "Account number must be Int",
+        statusText: "ACCOUNT_NOT_INT",
+      };
+    }
+
+    const accountHex = this.pad(account.toString(16), 8, 0);
+    const accountBuffer = Buffer.from(accountHex, "hex");
+    const p1 = 0x00;
+    const p2 = 0x00;
+    const statusList = [SW_OK, SW_CANCEL];
+
+    try {
+      const response = await this.transport.send(
+        CLA,
+        INS.GET_ADDR,
+        p1,
+        p2,
+        accountBuffer,
+        statusList
+      );
+
+      const publicKey = response.slice(0, response.length - 2).toString();
+      const returnCode = response
+        .slice(response.length - 2, response.length)
+        .toString("hex");
+
+      if (returnCode !== SW_OK.toString(16)) {
+        throw {
+          returnCode: returnCode,
+          message: "unknown error",
+          statusText: "UNKNOWN_ERROR",
+        };
+      }
+
+      return {
+        publicKey,
+        returnCode,
+      };
+    } catch (e) {
+      return {
+        publicKey: null,
+        returnCode: e.statusCode,
+        message: e.message,
+        statusText: e.statusText,
+      };
+    }
+  }
+
+  // /**
+  //  * Sign a Mina transaction with a given transaction
+  //  *
+  //  * @param Transaction arguments, see SignTransactionArgs type
+  //  * @return an object with signature and returnCode
+  //  */
+  async signTransaction({
+    txType,
+    senderAccount,
+    senderAddress,
+    receiverAddress,
+    amount,
+    fee,
+    nonce,
+    validUntil = 4294967295,
+    memo = "",
+    networkId,
+  }: SignTransactionArgs): Promise<SignTransactionResponse> {
+    if (
+      isNaN(txType) ||
+      isNaN(senderAccount) ||
+      !senderAddress ||
+      !receiverAddress ||
+      (!amount && txType === TxType.PAYMENT) ||
+      !fee ||
+      !Number.isInteger(amount) ||
+      !Number.isInteger(fee) ||
+      isNaN(nonce) ||
+      isNaN(networkId)
+    ) {
+      return {
+        signature: null,
+        returnCode: "-1",
+        message: "Missing or wrong arguments",
+        statusText: "MISSING_ARGUMENTS",
+      };
+    }
+    if (memo.length > 32) {
+      return {
+        signature: null,
+        returnCode: "-3",
+        message: "Memo field too long",
+        statusText: "MEMO_TOO_BIG",
+      };
+    }
+    if (fee < 1000000) {
+      return {
+        signature: null,
+        returnCode: "-4",
+        message: "Fee too small",
+        statusText: "FEE_TOO_SMALL",
+      };
+    }
+
+    // console.log(
+    //   txType,
+    //   senderAccount,
+    //   senderAddress,
+    //   receiverAddress,
+    //   amount,
+    //   fee,
+    //   nonce,
+    //   validUntil,
+    //   memo,
+    //   networkId
+    // );
+    const apdu = this.createTXApdu({
+      txType,
+      senderAccount,
+      senderAddress,
+      receiverAddress,
+      amount,
+      fee,
+      nonce,
+      validUntil,
+      memo,
+      networkId,
+    });
+    const apduBuffer = Buffer.from(apdu, "hex");
+    const p1 = 0x00;
+    const p2 = 0x00;
+    const statusList = [SW_OK, SW_CANCEL];
+
+    if (apduBuffer.length > 256) {
+      return {
+        signature: null,
+        returnCode: "-2",
+        message: "data length > 256 bytes",
+        statusText: "DATA_TOO_BIG",
+      };
+    }
+
+    try {
+      const response = await this.transport.send(
+        CLA,
+        INS.SIGN,
+        p1,
+        p2,
+        apduBuffer,
+        statusList
+      );
+
+      const signature = response.slice(0, response.length - 2).toString("hex");
+      const returnCode = response
+        .slice(response.length - 2, response.length)
+        .toString("hex");
+
+      return {
+        signature,
+        returnCode,
+      };
+    } catch (e) {
+      return {
+        signature: null,
+        returnCode: e.statusCode,
+        message: e.message,
+        statusText: e.statusText,
+      };
+    }
+  }
+
+  /**
+   * get the version of the Mina app installed on the hardware device
+   *
+   * @return an object with a version
+   */
+  async getAppVersion(): Promise<GetAppVersionesponse> {
+    try {
+      const response = await this.transport.send(
+        CLA,
+        INS.GET_VERSION,
+        0x00,
+        0x00
+      );
+      const versionRaw = response.slice(0, response.length - 2).toString("hex");
+      const version =
+        "" + versionRaw[1] + "." + versionRaw[2] + "." + versionRaw[3];
+      const returnCode = response
+        .slice(response.length - 2, response.length)
+        .toString("hex");
+
+      return {
+        version,
+        returnCode,
+      };
+    } catch (e) {
+      return {
+        version: null,
+        returnCode: e.statusCode,
+        message: e.message,
+        statusText: e.statusText,
+      };
+    }
+  }
+}
